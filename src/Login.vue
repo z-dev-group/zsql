28e12f5e60ae50c5b1b18553251fc5e9f9ffcca5
unknown <pant_ingwen@126.com>
2025-07-08 23:04:26 +0800

    save connection

--- src/Login.vue
+++ src/Login.vue
@@ -14,10 +14,15 @@
       <el-form-item label="密码">
         <el-input v-model="password" required />
       </el-form-item>
+      <el-form-item label="名称" placeholder="输入链接名会自动保存">
+        <el-input v-model="connection_name" />
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="handleLogin">连接</el-button>
       </el-form-item>
     </el-form>
+    <div>添加下面tag可以快速连接</div>
+    <el-tag style="cursor: pointer; margin-top: 10px;" v-for="item, name in connectionConfigs" :key="item" @click="handleSelectConnection(item)">{{ name }}</el-tag>
   </div>
 </template>
 
@@ -25,12 +30,21 @@
 import { ref } from 'vue';
 import { ElMessage } from 'element-plus';
 
-const host = ref('localhost');
-const port = ref('3307');
-const username = ref('root');
+const host = ref('');
+const port = ref('');
+const username = ref('');
 const password = ref('');
+const connection_name = ref('');
 import store from './store';
+const connectionConfigs = ref(localStorage.getItem("savedConnectionConfigs") ? JSON.parse(localStorage.getItem("savedConnectionConfigs")) : {});
 
+function handleSelectConnection(item) {
+  host.value = item.host;
+  port.value = item.port;
+  username.value = item.username;
+  password.value = item.password;
+  connection_name.value = name;
+}
 async function handleLogin() {
   // 验证表单
   if (!host.value || !port.value || !username.value) {
@@ -47,7 +61,12 @@ async function handleLogin() {
     port: port.value,
     username: username.value,
     password: password.value,
-    database: ''
+    database: '',
+  }
+  if (connection_name.value) {
+    let saveFileData = localStorage.getItem("savedConnectionConfigs") ? JSON.parse(localStorage.getItem("savedConnectionConfigs")) : {};
+    saveFileData[connection_name.value] = config;
+    localStorage.setItem("savedConnectionConfigs", JSON.stringify(saveFileData));
   }
   let result = await window.api.connectDatabase(config.host, config.port, config.username, config.password, config.database);
   console.log("result", result);
